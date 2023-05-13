3d1f81c7bf19a57655aaf2e917c216660bf8f95a
Bobby Palko <bobby@palko.dev>
2023-05-13 17:41:13 -0400

    started on love letters page

--- src/routes/love/+page.svelte
+++ src/routes/love/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Send a Love letter</h1>
 <p>Send an anonymous letter to the void. Or receive one back from the universe; who knows, maybe it's meant for you.</p>
+
+<form>    
+    <label class="label">
+        <span>Send A Message</span>
+        <textarea class="textarea" rows="4" placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit." />
+    </label>			
+    <button class="btn variant-filled-primary">Send</button>
+</form>
